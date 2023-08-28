89c54c51e7741484a5eaaa5646c08ab04719998b
lizzbejarano02 <lizzethbejarano620@gmail.com>
2023-08-28 12:23:15 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css'
+import NavBar from './components/NavBar'
+
+function App() {
+  
+
+  return (
+    <>
+      <div>
+       <NavBar/>
+       
+      </div>
+    </>
+  )
+}
+
+export default App
